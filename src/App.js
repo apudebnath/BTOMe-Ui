62d5396b92b4daf41d40b1d090241f0c74291d18
apudebnath <apu1578@gmail.com>
2022-01-02 00:08:52 +0600

    content updated in registration form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Registration from './Components/Login/Registration/Registration';
 
 function App() {
   return (
     <div className="App">
-      
+      <Registration/>
     </div>
   );
 }

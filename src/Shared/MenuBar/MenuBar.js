ae0fd4d8d14b86f524f8efad1ce6424f4aeff906
apudebnath <apu1578@gmail.com>
2022-01-02 23:29:32 +0600

    menubar updated

--- src/Shared/MenuBar/MenuBar.js
+++ src/Shared/MenuBar/MenuBar.js
@@ -14,18 +14,18 @@ const MenuBar = () => {
                             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                             <Navbar.Collapse id="responsive-navbar-nav">
                                 <Nav className="mx-auto">
-                                    <Nav.Link href="#home" className="me-2 fw-bold">Home</Nav.Link>
-                                    <Nav.Link href="#shop" className="me-2 fw-bold">Shop</Nav.Link>
-                                    <Nav.Link href="#blog" className="me-2 fw-bold">Blog</Nav.Link>
-                                    <Nav.Link href="#contact" className="me-2 fw-bold">Contact</Nav.Link>
-                                    <Nav.Link href="#about" className="me-2 fw-bold">About</Nav.Link>
+                                    <Nav.Link to="home" className="me-2 fw-bold">Home</Nav.Link>
+                                    <Nav.Link to="shop" className="me-2 fw-bold">Shop</Nav.Link>
+                                    <Nav.Link to="blog" className="me-2 fw-bold">Blog</Nav.Link>
+                                    <Nav.Link to="contact" className="me-2 fw-bold">Contact</Nav.Link>
+                                    <Nav.Link to="about" className="me-2 fw-bold">About</Nav.Link>
                                 </Nav>
                                 <Nav>
-                                    <Nav.Link href="#deets" className="me-2 "><i className="fas fa-search"></i></Nav.Link>
-                                    <Nav.Link href="#memes" className="me-2 ">
+                                    <Nav.Link to="deets" className="me-2 "><i className="fas fa-search"></i></Nav.Link>
+                                    <Nav.Link to="memes" className="me-2 ">
                                         <i className="fas fa-shopping-cart"></i>
                                     </Nav.Link>
-                                    <Nav.Link href="#user">
+                                    <Nav.Link to="user">
                                         <i className="fas fa-user"></i>
                                     </Nav.Link>
                                 </Nav>

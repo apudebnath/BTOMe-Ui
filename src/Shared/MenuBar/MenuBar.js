c86ac254db2df240a0e9d58af3f66c32c801c272
apudebnath <apu1578@gmail.com>
2022-01-02 23:16:28 +0600

    menubar updated

--- src/Shared/MenuBar/MenuBar.js
+++ src/Shared/MenuBar/MenuBar.js
@@ -6,27 +6,32 @@ const MenuBar = () => {
         <div>
             <Navbar collapseOnSelect expand="lg" >
                 <Container>
-                    <Navbar.Brand href="#home" className="fw-bold fs-3"><img src="./logo1.png" className="w-50" alt="" /></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mx-auto">
-                            <Nav.Link href="#home" className="me-2 fw-bold">Home</Nav.Link>
-                            <Nav.Link href="#shop" className="me-2 fw-bold">Shop</Nav.Link>
-                            <Nav.Link href="#blog" className="me-2 fw-bold">Blog</Nav.Link>
-                            <Nav.Link href="#contact" className="me-2 fw-bold">Contact</Nav.Link>
-                            <Nav.Link href="#about" className="me-2 fw-bold">About</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets" className="me-2 "><i className="fas fa-search"></i></Nav.Link>
-                            <Nav.Link href="#memes" className="me-2 ">
-                                <i className="fas fa-shopping-cart"></i>
-                            </Nav.Link>
-                            <Nav.Link href="#user">
-                                <i className="fas fa-user"></i>
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-
+                    <div className='row'>
+                        <div className='col-6 '>
+                            <Navbar.Brand href="#home" className="fw-bold fs-3"><img src="./logo1.png" className="w-50" alt="" /></Navbar.Brand>
+                        </div>
+                        <div className='col-6'>
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Nav className="mx-auto">
+                                    <Nav.Link href="#home" className="me-2 fw-bold">Home</Nav.Link>
+                                    <Nav.Link href="#shop" className="me-2 fw-bold">Shop</Nav.Link>
+                                    <Nav.Link href="#blog" className="me-2 fw-bold">Blog</Nav.Link>
+                                    <Nav.Link href="#contact" className="me-2 fw-bold">Contact</Nav.Link>
+                                    <Nav.Link href="#about" className="me-2 fw-bold">About</Nav.Link>
+                                </Nav>
+                                <Nav>
+                                    <Nav.Link href="#deets" className="me-2 "><i className="fas fa-search"></i></Nav.Link>
+                                    <Nav.Link href="#memes" className="me-2 ">
+                                        <i className="fas fa-shopping-cart"></i>
+                                    </Nav.Link>
+                                    <Nav.Link href="#user">
+                                        <i className="fas fa-user"></i>
+                                    </Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </div>
+                    </div>
                 </Container>
             </Navbar>
         </div>

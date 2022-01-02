d1aa4404fab66c17d1d633c6865d55533f6d9449
Shoieb Alam <shoieb.ctg@gmail.com>
2022-01-02 13:11:13 +0600

    about us updated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Col, Container, Row } from 'react-bootstrap';
+import SocialMedia from './SocialMedia';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGreaterThan } from '@fortawesome/free-solid-svg-icons';
+import './About.css';
 
 const About = () => {
     return (
@@ -7,12 +12,21 @@ const About = () => {
             <div>
                 <img className='h-25 w-100' src="https://i.ibb.co/7R56G9m/image.png" alt="" />
             </div>
-            <h2 className='py-5'>About</h2>
+
+            {/* Description part */}
             <div className='py-5'>
+                <div>
+                    <h2 className='border-bottom border-dark d-inline '> About</h2>
+                    <h6 className='py-3'>
+                        <Link className="text-decoration-none text-black" to='/'> Home</Link>
+                        <FontAwesomeIcon className="mx-2" icon={faGreaterThan} size="xs" />
+                        <span className="text-secondary"> About </span>
+                    </h6>
+                </div>
                 <Container>
-                    <Row className="d-flex align-items-center justify-content-md-center g-5">
+                    <Row className="d-flex align-items-center justify-content-md-center py-5">
                         <Col xs='12' md='6' lg="6">
-                            <img src="https://i.ibb.co/2y66jqn/image.png" alt="" />
+                            <img className='w-75' src="https://i.ibb.co/2y66jqn/image.png" alt="" />
                         </Col>
                         <Col xs='12' md='6' lg="6" className='text-start ps-5'>
                             <h4>Who we are</h4>
@@ -24,6 +38,8 @@ const About = () => {
                             </p>
                         </Col>
                     </Row>
+
+                    {/* Team part  */}
                     <div className='py-5'>
                         <h4>Meet Our Team</h4>
                         <p>
@@ -31,32 +47,57 @@ const About = () => {
                             The best people fomula for great websites!
                         </p>
                     </div>
-                    <Row xs={1} sm={2} md={3}>
+                    <Row xs={1} sm={2} md={4}>
+
                         <div>
-                            <img src="https://i.ibb.co/hcDHKb6/team5-1.jpg" alt="" />
-                            <p className='py-3'>
+                            <img className='h-50' src="https://i.ibb.co/0GbX4w0/image.png" alt="" />
+                            <p className='pt-3'>
+                                <b> Mark Jance </b> <br />
+                                <i> CEO / FOUNDER </i>
+                            </p>
+                            <SocialMedia />
+                        </div>
+
+                        <div>
+                            <img className='h-50' src="https://i.ibb.co/hcDHKb6/team5-1.jpg" alt="" />
+                            <p className='pt-3'>
                                 <b> Frank Greer </b> <br />
                                 <i> Operations </i>
                             </p>
+                            <SocialMedia />
                         </div>
 
                         <div>
-                            <img src="https://i.ibb.co/pvs0XsM/image.png" alt="" />
-                            <p className='py-3'>
+                            <img className='h-50' src="https://i.ibb.co/pvs0XsM/image.png" alt="" />
+                            <p className='pt-3'>
                                 <b> Perry Conner </b> <br />
                                 <i> Marketing Personal </i>
                             </p>
+                            <SocialMedia />
                         </div>
 
                         <div>
-                            <img src="https://i.ibb.co/VYMPpXF/team6.jpg" alt="" />
-                            <p className='py-3'>
+                            <img className='h-50' src="https://i.ibb.co/VYMPpXF/team6.jpg" alt="" />
+                            <p className='pt-3'>
                                 <b> Mark Tucker </b> <br />
                                 <i> Partner </i>
                             </p>
+                            <SocialMedia />
                         </div>
+
                     </Row>
                 </Container>
+            </div>
+            {/* brand logo part */}
+            <div>
+                <Row xs={2} sm={2} md={6} lg={6}>
+                    <img className='brand-logo' src="https://i.ibb.co/qjzvWPH/image.png" alt="" />
+                    <img className='brand-logo' src="https://i.ibb.co/ZXVFKyb/image.png" alt="" />
+                    <img className='brand-logo' src="https://i.ibb.co/ZVwtsk4/image.png" alt="" />
+                    <img className='brand-logo' src="https://i.ibb.co/N3YVRcn/image.png" alt="" />
+                    <img className='brand-logo' src="https://i.ibb.co/CHpZ4rQ/image.png" alt="" />
+                    <img className='brand-logo' src="https://i.ibb.co/qyf7y1Q/image.png" alt="" />
+                </Row>
             </div>
         </div>
     );

ec39d04508c5f79311bc783bed38833d6891e481
Shoieb Alam <shoieb.ctg@gmail.com>
2022-01-02 17:56:43 +0600

    contact us added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home/Home';
 import About from './Components/About/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from './Components/Contact/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="contact" element={<Contact />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;

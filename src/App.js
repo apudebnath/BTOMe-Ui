41ca094647f6a06759723908376fe10114093639
Shoieb Alam <shoieb.ctg@gmail.com>
2022-01-02 07:31:35 +0600

    about us added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home/Home';
+import About from './Components/About/About';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }

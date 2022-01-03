4ba7df153946de47b1c5161a84d8e5cd07c54f17
Shoieb Alam <shoieb.ctg@gmail.com>
2022-01-03 20:21:55 +0600

    dashboard added

--- src/Components/Dashboard/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard/Dashboard.js
@@ -7,16 +7,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Button } from '@mui/material';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 
 const drawerWidth = 240;
 
@@ -28,6 +24,15 @@ function Dashboard(props) {
         setMobileOpen(!mobileOpen);
     };
 
+    const buttons = [
+        <Button as={Link} to="/dashboard" key="one">Dashboard</Button>,
+        <Button as={Link} to="orders" key="two">Manage Orders</Button>,
+        <Button as={Link} to="messages" key="three">Users Messages</Button>,
+        <Button as={Link} to="addProduct" key="four">Add a Product</Button>,
+        <Button as={Link} to="manageProducts" key="five">Manage Products</Button>,
+        <Button as={Link} to="makeAdmin" key="six">Make An Admin</Button>,
+    ];
+
     const drawer = (
         <div>
             <Link to="/">
@@ -37,18 +42,16 @@ function Dashboard(props) {
                     className="d-inline-block align-top" alt="B2Me logo" />
             </Link>
             <Divider />
-            <Link to="/dashboard"> <Button color="inherit"> Dashboard </Button> </Link>
-            <br />
-            <Link to="orders"> <Button color="inherit">Manage Orders</Button> </Link>
-            <br />
-            <Link to="messages"> <Button color="inherit">Users Messages</Button> </Link>
-            <br />
-            <Link to="addProduct"> <Button color="inherit">Add a Product</Button> </Link>
-            <br />
-            <Link to="manageProducts"> <Button color="inherit">Manage Products</Button> </Link>
-            <br />
-            <Link to="makeAdmin"> <Button color="inherit">Make An Admin</Button> </Link>
-
+            <div className='py-5 ps-4'>
+                <ButtonGroup
+                    orientation="vertical"
+                    aria-label="vertical contained button group"
+                    variant="outlined"
+                    sx={{ p: 3 }}
+                >
+                    {buttons}
+                </ButtonGroup>
+            </div>
             <Divider />
         </div>
     );

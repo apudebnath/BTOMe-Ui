526ee99ea0c4f1209c72f945623ca011de581ca4
Shoieb Alam <shoieb.ctg@gmail.com>
2022-01-04 00:24:45 +0600

    products managing added

--- src/Components/Dashboard/AddProduct/AddProduct.js
+++ src/Components/Dashboard/AddProduct/AddProduct.js
@@ -7,7 +7,7 @@ const AddProduct = () => {
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
         console.log(data);
-        axios.post('', data)
+        axios.post('https://gentle-woodland-78175.herokuapp.com/add-product', data)
             .then(res => {
                 if (res.data.insertedId) {
                     alert('Product Added Successfully');
@@ -24,25 +24,49 @@ const AddProduct = () => {
 
                     <form onSubmit={handleSubmit(onSubmit)}>
 
-                        <input {...register("name", { required: true, maxLength: 50 })} className='w-50' placeholder="Product Name" />
+                        <input {...register("productName", { required: true, maxLength: 50 })} className='w-50' placeholder="Product Name" />
 
                         <div className="row d-flex justify-content-center">
                             <div className="col-md-5">
 
-                                <textarea {...register("description1")} placeholder="Short Description" />
+                                <select {...register("productCategory", { required: true })}>
+                                    <option value="" disabled selected hidden>Choose a Product Category</option>
+                                    <option value="Decoration Light">Decoration Light</option>
+                                    <option value="Skin Care">Skin Care</option>
+                                    <option value="Salwar Kameez">Salwar Kameez</option>
+                                    <option value="Watch">Watch</option>
+                                    <option value="T-Shirt">T-Shirt</option>
+                                    <option value="Jewelry">Jewelry</option>
+                                    <option value="Saree">Saree</option>
+                                    <option value="Hair Care">Hair Care</option>
+                                    <option value="Furniture">Furniture</option>
+                                    <option value="Garden Tools">Garden Tools</option>
+                                </select>
+
+                                <input {...register("productImage")} placeholder="Product Image URL" />
+
+                                <input type="number" {...register("productMainPrice", { required: true })} placeholder="Price" />
+
+                                {/* <textarea {...register("description1")} placeholder="Short Description" />
 
                                 <textarea {...register("description2")} placeholder="Details Description 1" />
 
-                                <textarea {...register("description3")} placeholder="Details Description 2" />
+                                <textarea {...register("description3")} placeholder="Details Description 2" /> */}
 
                             </div>
                             <div className="col-md-5">
-                                <input {...register("img1")} placeholder="Image 1 URL" />
 
+                                {/* 
                                 <input {...register("img2")} placeholder="Image 2 URL" />
 
-                                <input {...register("img3")} placeholder="Image 3 URL" />
-                                <input type="number" {...register("price", { required: true })} placeholder="Price" />
+                                <input {...register("img3")} placeholder="Image 3 URL" /> */}
+
+                                <input type="number" {...register("productOfferPrice")} placeholder="Discount Price" />
+
+                                <input type="number" {...register("productReview", { required: true })} placeholder="Product Review" />
+
+                                <input type="number" {...register("ReviewStar", { required: true })} placeholder="Review Star" />
+
                             </div>
 
                         </div>
--- src/Components/Dashboard/ManageProduct/ManageProduct.js
+++ src/Components/Dashboard/ManageProduct/ManageProduct.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
+import './ManageProduct.css';
 
 const ManageProduct = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://gentle-woodland-78175.herokuapp.com/all-products`)
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
+    const handleDelete = id => {
+        axios.delete(`https://gentle-woodland-78175.herokuapp.com/all-products/${id}`)
+            .then(res => {
+                if (res.data.deletedCount > 0) {
+                    alert('Are you sure to delete this Product?');
+                    console.log(res.data.deletedCount);
+                    console.log(res.data);
+                    const remaining = products.filter(product => product._id !== id);
+                    setProducts(remaining);
+                }
+            })
+    }
+
     return (
-        <div>
-            <h2>Manage Products</h2>
+        // Product Managing
+        <div className="manage-products">
+            <h2 className="py-5">Manage Products</h2>
+            {
+                products.map(product => <div key={product._id}>
+                    <Row className="m-2 bg-secondary rounded-pill w-75 mx-auto p-3 text-white">
+                        <Col className='my-auto'>
+                            <img className='w-25 rounded-pill' src={product.productImage} alt="" />
+                        </Col>
+                        <Col className='my-auto'>
+                            <h5 className="text-start"> {product.productName} </h5>
+                        </Col>
+                        <Col className='my-auto'>
+                            <Button variant="outline-danger" className="btn-dark" onClick={() => { handleDelete(product._id) }}>Delete</Button>
+                        </Col>
+                    </Row>
+                </div>)
+            }
         </div>
     );
 };
